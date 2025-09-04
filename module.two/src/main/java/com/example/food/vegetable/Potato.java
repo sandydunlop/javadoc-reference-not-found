ffd8e142c348a9667702c851f8646fb2a1bda2a1
Sandy Dunlop <sandy.dunlop@gmail.com>
2025-09-04 19:50:12 +0100

    changed a class name

--- module.two/src/main/java/com/example/food/vegetable/Potato.java
+++ module.two/src/main/java/com/example/food/vegetable/Potato.java
@@ -2,16 +2,16 @@ package com.example.food.vegetable;
 
 /// Example class
 /// [module.one/com.example.botany.fruit.Tomato] is a reference to a class
-public class Tomato {
+public class Potato {
 
     /// Default constructor
-    public Tomato() {
+    public Potato() {
         // Only here for the comment
     }
 
     /// simple method
-    /// @return always returns red
+    /// @return always returns white
     public String color() {
-        return "red";
+        return "white";
     }
 }
